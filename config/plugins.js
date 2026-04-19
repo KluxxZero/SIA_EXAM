b6324a74a4eb9ec98c9cef316674a5e4b60aceee
Jwshux <joshuariana05@gmail.com>
2026-04-19 23:30:31 +0800

    Set jwtSecret for users-permissions plugin

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,6 @@
-module.exports = () => ({});
+// config/plugins.js
+module.exports = {
+  'users-permissions': {
+    jwtSecret: process.env.JWT_SECRET || 'your-jwt-secret-key', // You can replace with your custom secret
+  },
+};
